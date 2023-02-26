dc7c55483cbc313db94c285fe39cfaf1f82eb472
KoushaAmouzesh <kousha82@@students.cs.ubc.ca>
2023-02-25 18:06:21 -0800

    made a backend with Api call but needs to use firebase with jobs model

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 const express = require('express');
 
+const fs = require('fs');
+
 const app = express();
+
+const axios = require('axios');
+
+const dotenv = require('dotenv');
+dotenv.config({path: './key.env'});
+
+
+// load the key
+const key = process.env.API_KEY;
+
+
 
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 
+async function getJobs() {
+    try {
+        response = await axios.get('https://serpapi.com/search.json?engine=google_jobs&q=accountant+vancouver&api_key=' + key + '&hl=en');
+        console.log(response.data);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 //index page 
 app.get('/', function(req, res) {
     try {
         res.render('pages/index');
+        getJobs();
     } catch (error){
         console.log(error);
         res.send("Error while fetching");
@@ -18,11 +42,24 @@ app.get('/', function(req, res) {
     
 });
 
+
 // about page
 app.get('/about', function(req, res) {
-    res.render('pages/about');
+    fs.readFile('about.txt', 'utf8', (err, data) => {
+        try {
+            res.render('pages/about', { text: data });
+        } catch (error) {
+            console.log(error);
+        }
+        
+      });
 });
-  
-app.listen(8080);
 
-console.log("listening to port: http://localhost:"+"8080");
+
+
+
+  
+const port = 8080;
+app.listen(port);
+
+console.log("listening to port: http://localhost:"+port);
